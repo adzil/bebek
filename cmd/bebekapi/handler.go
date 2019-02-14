3e7b826529a30570bc2df47bae5ce100fc8f8b71
Yusuf Rahmatullah <yusuf.rahmatullah@bukalapak.com>
2019-02-14 07:52:41 +0700

    Implement Handler Change type of Date using custom type

--- cmd/bebekapi/handler.go
+++ cmd/bebekapi/handler.go
@@ -1,35 +1,204 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"time"
+
+	"github.com/bukalapak/apinizer/response"
+
+	"github.com/adzil/bebek"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 // Handler is the room booking HTTP handler.
-type Handler struct{}
+type Handler struct {
+	Service bebek.Service
+}
+
+// ArrayMeta is response meta data for array of object
+type ArrayMeta struct {
+	HTTPStatus int `json:"http_status"`
+	Length     int `json:"length"`
+}
 
 // GetRooms serves GET /rooms endpoint.
 func (h *Handler) GetRooms(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-
+	rooms, err := h.Service.GetRooms()
+	if err != nil {
+		writeResponseError(w, err)
+	} else {
+		meta := newArrayMeta(len(rooms))
+		sucResp := response.BuildSuccess(rooms, "", meta)
+		response.Write(w, sucResp, http.StatusOK)
+	}
 }
 
 // GetBookings serves GET /bookings endpoint.
 func (h *Handler) GetBookings(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-
+	req, errResp := getReversationRequestFromQuery(r)
+	if errResp != nil {
+		response.Write(w, errResp, http.StatusBadRequest)
+		return
+	}
+	resv, err := h.Service.GetReservations(*req)
+	if err != nil {
+		writeResponseError(w, err)
+	} else {
+		meta := newArrayMeta(len(resv))
+		sucResp := response.BuildSuccess(resv, "", meta)
+		response.Write(w, sucResp, http.StatusOK)
+	}
 }
 
 // PostBooking serves POST /bookings endpoint.
 func (h *Handler) PostBooking(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-
+	bookingReq, errResp := createBookingRequestFromBody(r)
+	if errResp != nil {
+		response.Write(w, errResp, http.StatusBadRequest)
+		return
+	}
+	bookingID, err := h.Service.CreateBooking(*bookingReq)
+	if err != nil {
+		writeResponseError(w, err)
+		return
+	}
+	meta := struct {
+		HTTPStatus int `json:"http_status"`
+	}{
+		HTTPStatus: http.StatusOK,
+	}
+	resp := struct {
+		BookingID string `json:"booking_id"`
+	}{
+		BookingID: bookingID,
+	}
+	sucResp := response.BuildSuccess(resp, "", meta)
+	response.Write(w, sucResp, http.StatusOK)
 }
 
 // DeleteBooking serves DELETE /bookings endpoint.
 func (h *Handler) DeleteBooking(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-
+	bookingID := params.ByName("id")
+	actor := r.Header.Get("X-Telegram-User")
+	if actor == "" {
+		errResp := buildErrorResponse("header", "User is not set")
+		response.Write(w, errResp, http.StatusBadRequest)
+		return
+	}
+	req := bebek.DeleteBookingRequest{
+		Actor:     actor,
+		BookingID: bookingID,
+	}
+	err := h.Service.DeleteBooking(req)
+	if err != nil {
+		writeResponseError(w, err)
+		return
+	}
+	meta := struct {
+		HTTPStatus int `json:"http_status"`
+	}{
+		HTTPStatus: http.StatusOK,
+	}
+	sucResp := response.BuildSuccess(nil, "Delete Booking successful", meta)
+	response.Write(w, sucResp, http.StatusOK)
 }
 
 // GetSelfBookings serves GET /self/bookings endpoint.
 func (h *Handler) GetSelfBookings(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	actor := r.Header.Get("X-Telegram-User")
+	if actor == "" {
+		errResp := buildErrorResponse("header", "User is not set")
+		response.Write(w, errResp, http.StatusBadRequest)
+		return
+	}
+	req := bebek.GetSelfReservationsRequest{
+		Actor: actor,
+	}
+	resv, err := h.Service.GetSelfReservations(req)
+	if err != nil {
+		writeResponseError(w, err)
+		return
+	}
+	meta := newArrayMeta(len(resv))
+	sucResp := response.BuildSuccess(nil, "Delete Booking successful", meta)
+	response.Write(w, sucResp, http.StatusOK)
+}
 
+func buildErrorResponse(field, msg string) response.ErrorBody {
+	return response.BuildError(response.ErrorInfo{
+		Code:    http.StatusBadRequest,
+		Field:   field,
+		Message: msg,
+	})
+}
+
+func createBookingRequestFromBody(r *http.Request) (*bebek.CreateBookingRequest, *response.ErrorBody) {
+	if r.Body == nil {
+		errResp := buildErrorResponse("body", "POST body is empty")
+		return nil, &errResp
+	}
+	var bookingReq bebek.CreateBookingRequest
+	err := json.NewDecoder(r.Body).Decode(&bookingReq)
+	if err != nil {
+		errResp := buildErrorResponse("body", err.Error())
+		return nil, &errResp
+	}
+	if valid := validateCreateBookingRequest(bookingReq); !valid {
+		errResp := buildErrorResponse("body", "POST body is invalid")
+		return nil, &errResp
+	}
+	actor := r.Header.Get("X-Telegram-User")
+	if actor == "" {
+		errResp := buildErrorResponse("header", "User is not set")
+		return nil, &errResp
+	}
+	bookingReq.Actor = actor
+	return &bookingReq, nil
+}
+
+func getReversationRequestFromQuery(r *http.Request) (*bebek.GetReservationsRequest, *response.ErrorBody) {
+	query := r.URL.Query()
+	roomID := query.Get("room_id")
+	dateStr := query.Get("date")
+	date := time.Now()
+	if dateStr != "" {
+		var err error
+		date, err = time.Parse(bebek.DateLayout, dateStr)
+		if err != nil {
+			errResp := buildErrorResponse("date", "date format is invalid")
+			return nil, &errResp
+		}
+	}
+	return &bebek.GetReservationsRequest{
+		Date:   bebek.Date{Time: date},
+		RoomID: roomID,
+	}, nil
+}
+
+func newArrayMeta(length int) *ArrayMeta {
+	return &ArrayMeta{
+		HTTPStatus: http.StatusOK,
+		Length:     length,
+	}
+}
+
+func validateCreateBookingRequest(req bebek.CreateBookingRequest) bool {
+	if req.RoomID == "" {
+		return false
+	}
+	today := time.Now().Truncate(24 * time.Hour)
+	if req.Date.Before(today) {
+		return false
+	}
+	if req.End < req.Start {
+		return false
+	}
+	return true
+}
+
+func writeResponseError(w http.ResponseWriter, err error) {
+	errResp := response.BuildError(err)
+	response.Write(w, errResp, http.StatusInternalServerError)
 }
--- type.go
+++ type.go
@@ -1,6 +1,20 @@
 package bebek
 
-import "time"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+// DateLayout is time format used for parsing date string
+const DateLayout = "020106"
+
+var nilTime = (time.Time{}).UnixNano()
+
+// Date is time.Time which layout using DateLayout
+type Date struct {
+	time.Time
+}
 
 // Room represents the room information.
 type Room struct {
@@ -11,16 +25,40 @@ type Room struct {
 
 // Booking represents the booking information.
 type Booking struct {
-	BookingID  string    `json:"booking_id"`
-	RoomID     string    `json:"-"`
-	Date       time.Time `json:"date"`
-	Start      int       `json:"start"`
-	End        int       `json:"end"`
-	ReservedBy string    `json:"reserved_by"`
+	BookingID  string `json:"booking_id"`
+	RoomID     string `json:"-"`
+	Date       Date   `json:"date"`
+	Start      int    `json:"start"`
+	End        int    `json:"end"`
+	ReservedBy string `json:"reserved_by"`
 }
 
 // Reservation represents the room and booking information.
 type Reservation struct {
 	*Room
 	Booking []*Booking `json:"slots"`
+}
+
+// UnmarshalJSON decode Date as JSON
+func (d *Date) UnmarshalJSON(b []byte) (err error) {
+	s := strings.Trim(string(b), "\"")
+	if s == "null" {
+		d.Time = time.Time{}
+		return
+	}
+	d.Time, err = time.Parse(DateLayout, s)
+	return
+}
+
+// MarshalJSON encode Date to JSON
+func (d *Date) MarshalJSON() ([]byte, error) {
+	if d.Time.UnixNano() == nilTime {
+		return []byte("null"), nil
+	}
+	return []byte(fmt.Sprintf("\"%s\"", d.Time.Format(DateLayout))), nil
+}
+
+// IsSet check whether is value of Date has been set
+func (d *Date) IsSet() bool {
+	return d.UnixNano() != nilTime
 }
